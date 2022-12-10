931d5bea0c7901fe7bcf45957114c1aa476364f0
Thong Hua <huavanthong14@gmail.com>
2022-12-10 13:19:11 +0700

    Add backup version 3.

--- 01_GettingStarted/03_PostgreSQL/data_test.sql
+++ 01_GettingStarted/03_PostgreSQL/data_test.sql
@@ -5,3 +5,7 @@ INSERT INTO tbl2SubCategories (subcategoryid, SubCategoryName, Description, Cate
 (1,'Coffees', 'Product related to coffee', 1),
 (2, 'Teas', 'Product related to teas', 1),
 (3,'Beers', 'Product related to beers', 1);
+
+INSERT INTO tbl1customers (CustomerID ,CustomerName ,ContactName ,Address ,City ,PostalCode ,Country ,EmployeeID) VALUES
+(94, Bosch, bosch.com.vn, eTower Truong Chinh Quan Tan Binh	Ho Chi Minh, 008428, Viet Nam)
+(95, Bosch, bosch2.com.vn, eTower2 Truong Chinh Quan Tan Binh	Ho Chi Minh, 008428, Viet Nam)
